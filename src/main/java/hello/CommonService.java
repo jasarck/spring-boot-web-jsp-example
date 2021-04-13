98009d602e02893892d0f6eac3dc3500ce21e924
jasarck <jasirclt@gmail.com>
2021-04-13 10:36:58 +0530

    Update CommonService.java

--- src/main/java/hello/CommonService.java
+++ src/main/java/hello/CommonService.java
@@ -10,7 +10,6 @@ public interface CommonService {
 
 
     public boolean isAuthKeyValid(String authKey) throws SQLException;
-    public void updateRoomStatus(int roomId,String roomStatus,int expiry) throws SQLException;
     public String getUserIdByAuthKey(String authKey) throws SQLException;
 
 }
